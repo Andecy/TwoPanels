7c871bb8572be84168aab8284286d3ea4af78a30
Andecy <andecy@foxmail.com>
2016-06-07 14:42:02 +0800

    调整代码格式

--- libraryTwoPanels/src/main/java/com/desarrollodroide/twopanels/LeftFragment.java
+++ libraryTwoPanels/src/main/java/com/desarrollodroide/twopanels/LeftFragment.java
@@ -12,93 +12,94 @@ import android.widget.RelativeLayout;
 
 public class LeftFragment extends Fragment {
 
-	private ImageButton mSlideLeftButton;
-	public View mContainer;
-	private Boolean mSlidersButtonActive = true;
-	private int mDrawableLeft = R.drawable.ic_slider_left;
-	private int mDrawableUp = R.drawable.ic_slider_top;
+    private ImageButton mSlideLeftButton;
+    public View mContainer;
+    private Boolean mSlidersButtonActive = true;
+    private int mDrawableLeft = R.drawable.ic_slider_left;
+    private int mDrawableUp = R.drawable.ic_slider_top;
 
-	public interface OnSliderLeftListener {
-		/** Called by LeftFragment when sliderLeftButton is clicked */
-		public void slideFragmentsToLeft();
-	}
+    public interface OnSliderLeftListener {
+        /**
+         * Called by LeftFragment when sliderLeftButton is clicked
+         */
+        public void slideFragmentsToLeft();
+    }
 
-	OnSliderLeftListener mCallback;
+    OnSliderLeftListener mCallback;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		try {
-			mCallback = (OnSliderLeftListener) getActivity();
-		} catch (ClassCastException e) {
-			throw new ClassCastException(getActivity().toString() + " must implement OnSliderLeftListener");
-		}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        try {
+            mCallback = (OnSliderLeftListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getActivity().toString() + " must implement OnSliderLeftListener");
+        }
 
-		View myFragmentView = inflater.inflate(R.layout.left_fragment, container, false);
-		mSlideLeftButton = (ImageButton) myFragmentView.findViewById(R.id.slideLeftButton);
-		mSlideLeftButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				mCallback.slideFragmentsToLeft();
-			}
-		});
+        View myFragmentView = inflater.inflate(R.layout.left_fragment, container, false);
+        mSlideLeftButton = (ImageButton) myFragmentView.findViewById(R.id.slideLeftButton);
+        mSlideLeftButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mCallback.slideFragmentsToLeft();
+            }
+        });
 
-		if (((TwoPanelsBaseActivity) getActivity()).getFragmentsOrientation() == LinearLayout.VERTICAL) {
-			updateSlideLeftButtonOrientationVertical();
-			mSlideLeftButton.setImageResource(mDrawableUp);
+        if (((TwoPanelsBaseActivity) getActivity()).getFragmentsOrientation() == LinearLayout.VERTICAL) {
+            updateSlideLeftButtonOrientationVertical();
+            mSlideLeftButton.setImageResource(mDrawableUp);
 
-		} else {
-			// Define in XML
-			mSlideLeftButton.setImageResource(mDrawableLeft);
-		}
-		this.mContainer = myFragmentView;
-		return myFragmentView;
-	}
+        } else {
+            // Define in XML
+            mSlideLeftButton.setImageResource(mDrawableLeft);
+        }
+        this.mContainer = myFragmentView;
+        return myFragmentView;
+    }
 
-	
-	protected void setSliderButtonsDrawables(int up, int left) {
-		mDrawableUp = up;
-		mDrawableLeft = left;
-		if (((TwoPanelsBaseActivity) getActivity()).getFragmentsOrientation() == LinearLayout.VERTICAL) {
-			mSlideLeftButton.setImageResource(mDrawableUp);
-		} else {
-			mSlideLeftButton.setImageResource(mDrawableLeft);
-		}
-	}
 
-	public void updateSlideLeftButtonOrientationVertical() {
-		if (mSlidersButtonActive) {
-			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-			params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-			mSlideLeftButton.setLayoutParams(params);
-			mSlideLeftButton.setImageResource(mDrawableUp);
-		}
-	}
+    protected void setSliderButtonsDrawables(int up, int left) {
+        mDrawableUp = up;
+        mDrawableLeft = left;
+        if (((TwoPanelsBaseActivity) getActivity()).getFragmentsOrientation() == LinearLayout.VERTICAL) {
+            mSlideLeftButton.setImageResource(mDrawableUp);
+        } else {
+            mSlideLeftButton.setImageResource(mDrawableLeft);
+        }
+    }
 
-	public void updateSlideLeftButtonOrientationHorizontal() {
-		if (mSlidersButtonActive) {
+    public void updateSlideLeftButtonOrientationVertical() {
+        if (mSlidersButtonActive) {
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            mSlideLeftButton.setLayoutParams(params);
+            mSlideLeftButton.setImageResource(mDrawableUp);
+        }
+    }
 
-			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-			params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-			mSlideLeftButton.setLayoutParams(params);
-			mSlideLeftButton.setImageResource(mDrawableLeft);
-		}
-	}
+    public void updateSlideLeftButtonOrientationHorizontal() {
+        if (mSlidersButtonActive) {
 
-	public void switchButtonsSliderVisivility() {
-		if (mSlidersButtonActive) {
-			mSlideLeftButton.setVisibility(View.GONE);
-			mSlidersButtonActive = false;
-		} else {
-			mSlideLeftButton.setVisibility(View.VISIBLE);
-			mSlidersButtonActive = true;
-		}
-	}
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            mSlideLeftButton.setLayoutParams(params);
+            mSlideLeftButton.setImageResource(mDrawableLeft);
+        }
+    }
 
-	public ImageButton getmSlideLeftButton() {
-		return mSlideLeftButton;
-	}
+    public void switchButtonsSliderVisivility() {
+        if (mSlidersButtonActive) {
+            mSlideLeftButton.setVisibility(View.GONE);
+            mSlidersButtonActive = false;
+        } else {
+            mSlideLeftButton.setVisibility(View.VISIBLE);
+            mSlidersButtonActive = true;
+        }
+    }
 
+    public ImageButton getmSlideLeftButton() {
+        return mSlideLeftButton;
+    }
 
 
 }
--- libraryTwoPanels/src/main/java/com/desarrollodroide/twopanels/MyExtendsFrameLayout.java
+++ libraryTwoPanels/src/main/java/com/desarrollodroide/twopanels/MyExtendsFrameLayout.java
@@ -4,31 +4,31 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
-public  class MyExtendsFrameLayout extends FrameLayout {
-	public MyExtendsFrameLayout(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public MyExtendsFrameLayout(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public MyExtendsFrameLayout(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		// TODO Auto-generated constructor stub
-	}
+public class MyExtendsFrameLayout extends FrameLayout {
+    public MyExtendsFrameLayout(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
 
-	@SuppressWarnings("unused")
-	private void setMyWidth(int value) {
-		getLayoutParams().width = value;
-		requestLayout();
-	}
-	
-	@SuppressWarnings("unused")
-	private void setMyHeight(int value) {
-		getLayoutParams().height = value;
-		requestLayout();
-	}
+    public MyExtendsFrameLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+    }
+
+    public MyExtendsFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+    @SuppressWarnings("unused")
+    private void setMyWidth(int value) {
+        getLayoutParams().width = value;
+        requestLayout();
+    }
+
+    @SuppressWarnings("unused")
+    private void setMyHeight(int value) {
+        getLayoutParams().height = value;
+        requestLayout();
+    }
 }
