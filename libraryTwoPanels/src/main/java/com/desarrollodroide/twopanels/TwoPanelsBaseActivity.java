2add9c9a28195b996b039ee1a623e6143a46c473
Andecy <andecy@foxmail.com>
2016-06-07 14:34:57 +0800

    调整代码布局、优化子控件搜寻方式、为RightFragment加入滚动视图

--- libraryTwoPanels/src/main/java/com/desarrollodroide/twopanels/TwoPanelsBaseActivity.java
+++ libraryTwoPanels/src/main/java/com/desarrollodroide/twopanels/TwoPanelsBaseActivity.java
@@ -11,212 +11,214 @@ import android.widget.LinearLayout;
 
 public abstract class TwoPanelsBaseActivity extends FragmentActivity implements LeftFragment.OnSliderLeftListener, RightFragment.OnSliderRightListener {
 
-	protected Fragment mRightFragment = new Fragment();
-	protected Fragment mLeftFragment = new Fragment();
-	private TwoPaneLayout mRootPanel = null;
-	private Boolean mIsLeftShowing = true;
-	private Boolean mIsRightShowing = true;
+    protected Fragment mRightFragment = new Fragment();
+    protected Fragment mLeftFragment = new Fragment();
+    private TwoPaneLayout mRootPanel = null;
+    private Boolean mIsLeftShowing = true;
+    private Boolean mIsRightShowing = true;
 
-	// Default weight of fragments
-	protected float mLeftWeight = 0.30f;
-	protected float mRightWeight = 0.70f;
+    // Default weight of fragments
+    protected float mLeftWeight = 0.30f;
+    protected float mRightWeight = 0.70f;
 
-	// Default orientation
-	private int mFragmentsOrientation = LinearLayout.HORIZONTAL;
+    // Default orientation
+    private int mFragmentsOrientation = LinearLayout.HORIZONTAL;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.two_panels_activity_main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two_panels_activity_main);
 
-		if (savedInstanceState != null) {
-			// Restore value of members from saved state
-			mIsLeftShowing = savedInstanceState.getBoolean("isLeftShowing");
-			mIsRightShowing = savedInstanceState.getBoolean("isRightShowing");
-		}
+        mRootPanel = (TwoPaneLayout) findViewById(R.id.root);
+        mRootPanel.setOrientation(mFragmentsOrientation);
 
-		mRootPanel = (TwoPaneLayout) findViewById(R.id.root);
-		mRootPanel.setOrientation(mFragmentsOrientation);
+        if (mFragmentsOrientation == LinearLayout.VERTICAL) {
+            mRootPanel.setHeightsFromWeight(mLeftWeight, mRightWeight);
+        } else {
+            mRootPanel.setWidthsFromWeight(mLeftWeight, mRightWeight);
+        }
+    }
 
-		if (mFragmentsOrientation == LinearLayout.VERTICAL) {
-			mRootPanel.setHeightsFromWeight(mLeftWeight, mRightWeight);
-		} else {
-			mRootPanel.setWidthsFromWeight(mLeftWeight, mRightWeight);
-		}
-	}
+    protected void switchSliderVisitility() {
+        if (mIsLeftShowing && mIsRightShowing) {
+            mRootPanel.changeSliderVisitility();
+        }
+    }
 
-	protected void switchSliderVisitility() {
-		if (mIsLeftShowing && mIsRightShowing) {
-			mRootPanel.changeSliderVisitility();
-		}
-	}
-	
-	protected void setSliderVisitility (Boolean visibility){
-		mRootPanel.setSliderVisitility(visibility);
-	}
+    protected void setSliderVisitility(Boolean visibility) {
+        mRootPanel.setSliderVisitility(visibility);
+    }
 
-	protected void setBaseOrientation(int orientation) {
-		if (orientation == LinearLayout.HORIZONTAL) {
-			if (mFragmentsOrientation == LinearLayout.VERTICAL) {
-				changeOrientation(LinearLayout.HORIZONTAL);
-			}
-		} else if (orientation == LinearLayout.VERTICAL) {
-			if (mFragmentsOrientation == LinearLayout.HORIZONTAL) {
-				changeOrientation(LinearLayout.VERTICAL);
-			}
-		}
-	}
+    protected void setBaseOrientation(int orientation) {
+        if (orientation == LinearLayout.HORIZONTAL) {
+            if (mFragmentsOrientation == LinearLayout.VERTICAL) {
+                changeOrientation(LinearLayout.HORIZONTAL);
+            }
+        } else if (orientation == LinearLayout.VERTICAL) {
+            if (mFragmentsOrientation == LinearLayout.HORIZONTAL) {
+                changeOrientation(LinearLayout.VERTICAL);
+            }
+        }
+    }
 
-	public void setSlidersDrawables(int verticalDrawable, int horizontalDrawable) {
-		mRootPanel.setSlidersDrawables(verticalDrawable, horizontalDrawable);
-	}
+    public void setSlidersDrawables(int verticalDrawable, int horizontalDrawable) {
+        mRootPanel.setSlidersDrawables(verticalDrawable, horizontalDrawable);
+    }
 
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-		updateButtonSliderOrientation();
-		mRootPanel.updateWidgetsOnOrientationChange(mIsLeftShowing, mIsRightShowing, true);
-	}
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        updateButtonSliderOrientation();
+        mRootPanel.updateWidgetsOnOrientationChange(mIsLeftShowing, mIsRightShowing, true);
+    }
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		// Saving state for rotations
-		outState.putBoolean("isLeftShowing", mIsLeftShowing);
-		outState.putBoolean("isRightShowing", mIsRightShowing);
-		super.onSaveInstanceState(outState);
-	}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Saving state for rotations
+        outState.putBoolean("isLeftShowing", mIsLeftShowing);
+        outState.putBoolean("isRightShowing", mIsRightShowing);
+        super.onSaveInstanceState(outState);
+    }
 
-	@Override
-	public void slideFragmentsToLeft() {
-		if (mIsLeftShowing && mIsRightShowing) {
-			mRootPanel.hideLeft();
-			mIsLeftShowing = false;
-			mIsRightShowing = true;
-		} else if (mIsLeftShowing) {
-			mRootPanel.showRight();
-			mIsRightShowing = true;
-			mIsLeftShowing = true;
-		}
-	}
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore value of members from saved state
+        mIsLeftShowing = savedInstanceState.getBoolean("isLeftShowing");
+        mIsRightShowing = savedInstanceState.getBoolean("isRightShowing");
+    }
 
-	@Override
-	public void slideFragmentsToRight() {
-		if (mIsRightShowing && mIsLeftShowing) {
-			mRootPanel.hideRight();
-			mIsRightShowing = false;
-			mIsLeftShowing = true;
-		} else if (mIsRightShowing) {
-			mRootPanel.showLeft();
-			mIsRightShowing = true;
-			mIsLeftShowing = true;
-		}
-	}
+    @Override
+    public void slideFragmentsToLeft() {
+        if (mIsLeftShowing && mIsRightShowing) {
+            mRootPanel.hideLeft();
+            mIsLeftShowing = false;
+            mIsRightShowing = true;
+        } else if (mIsLeftShowing) {
+            mRootPanel.showRight();
+            mIsRightShowing = true;
+            mIsLeftShowing = true;
+        }
+    }
 
-	public void hideLeft() {
-		if (mIsLeftShowing) {
-			mRootPanel.hideLeftNoAnimate();
-			mIsLeftShowing = false;
-			mIsRightShowing = true;
-		}
-	}
+    @Override
+    public void slideFragmentsToRight() {
+        if (mIsRightShowing && mIsLeftShowing) {
+            mRootPanel.hideRight();
+            mIsRightShowing = false;
+            mIsLeftShowing = true;
+        } else if (mIsRightShowing) {
+            mRootPanel.showLeft();
+            mIsRightShowing = true;
+            mIsLeftShowing = true;
+        }
+    }
 
-	public void hideRight() {
-		if (mIsRightShowing) {
-			mRootPanel.hideRightNoAnimate();
-			mIsLeftShowing = true;
-			mIsRightShowing = false;
-		}
-	}
+    public void hideLeft() {
+        if (mIsLeftShowing) {
+            mRootPanel.hideLeftNoAnimate();
+            mIsLeftShowing = false;
+            mIsRightShowing = true;
+        }
+    }
 
-	public void showTwoFragments() {
-		mRootPanel.showTwoPanels();
-		mIsLeftShowing = true;
-		mIsRightShowing = true;
-	}
+    public void hideRight() {
+        if (mIsRightShowing) {
+            mRootPanel.hideRightNoAnimate();
+            mIsLeftShowing = true;
+            mIsRightShowing = false;
+        }
+    }
 
-	// Called when finish onCreate()
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		mRightFragment = getSupportFragmentManager().findFragmentById(R.id.right);
-		mLeftFragment = getSupportFragmentManager().findFragmentById(R.id.left);
-	}
+    public void showTwoFragments() {
+        mRootPanel.showTwoPanels();
+        mIsLeftShowing = true;
+        mIsRightShowing = true;
+    }
 
-	private void changeOrientation(int orientation) {
-		mFragmentsOrientation = orientation;
-		mRootPanel.setOrientation(orientation);
-		mRootPanel.setParamsValues();
-		updateButtonSliderOrientation();
-		mRootPanel.updateWidgetsOnOrientationChange(mIsLeftShowing, mIsRightShowing, false);
-	}
+    // Called when finish onCreate()
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        mRightFragment = getSupportFragmentManager().findFragmentById(R.id.right);
+        mLeftFragment = getSupportFragmentManager().findFragmentById(R.id.left);
+    }
 
-	public int getStatusBarHeight() {
-		int result = 0;
-		int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-		if (resourceId > 0) {
-			result = getResources().getDimensionPixelSize(resourceId);
-		}
-		return result;
-	}
+    private void changeOrientation(int orientation) {
+        mFragmentsOrientation = orientation;
+        mRootPanel.setOrientation(orientation);
+        mRootPanel.setParamsValues();
+        updateButtonSliderOrientation();
+        mRootPanel.updateWidgetsOnOrientationChange(mIsLeftShowing, mIsRightShowing, false);
+    }
 
-	public int getScreenHeight() {
-		DisplayMetrics displaymetrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-		return displaymetrics.heightPixels;
-	}
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
 
-	public int getScreenWidth() {
-		DisplayMetrics displaymetrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-		return displaymetrics.widthPixels;
-	}
+    public int getScreenHeight() {
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        return displaymetrics.heightPixels;
+    }
 
-	public int getActionBarHeight() {
-		TypedValue tv = new TypedValue();
-		if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
-			return TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
-		} else {
-			return 0;
-		}
-	}
+    public int getScreenWidth() {
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        return displaymetrics.widthPixels;
+    }
 
-	public void updateButtonSliderOrientation() {
-		// Manage errors if fragments not extends from RightFragment and
-		// LeftFragment
-		try {
-			if (getFragmentsOrientation() == LinearLayout.VERTICAL) {
-				((LeftFragment) mLeftFragment).updateSlideLeftButtonOrientationVertical();
-				((RightFragment) mRightFragment).updateSlideRightButtonOrientationVertical();
-			} else {
-				((LeftFragment) mLeftFragment).updateSlideLeftButtonOrientationHorizontal();
-				((RightFragment) mRightFragment).updateSlideRightButtonOrientationHorizontal();
-			}
-		} catch (Exception e) {
-			Log.e("updateButtonSlider", e.toString());
-		}
-	}
+    public int getActionBarHeight() {
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            return TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+        } else {
+            return 0;
+        }
+    }
 
-	public Fragment getmRightFragment() {
-		return mRightFragment;
-	}
+    public void updateButtonSliderOrientation() {
+        // Manage errors if fragments not extends from RightFragment and
+        // LeftFragment
+        try {
+            if (getFragmentsOrientation() == LinearLayout.VERTICAL) {
+                ((LeftFragment) mLeftFragment).updateSlideLeftButtonOrientationVertical();
+                ((RightFragment) mRightFragment).updateSlideRightButtonOrientationVertical();
+            } else {
+                ((LeftFragment) mLeftFragment).updateSlideLeftButtonOrientationHorizontal();
+                ((RightFragment) mRightFragment).updateSlideRightButtonOrientationHorizontal();
+            }
+        } catch (Exception e) {
+            Log.e("updateButtonSlider", e.toString());
+        }
+    }
 
-	public Fragment getmLeftFragment() {
-		return mLeftFragment;
-	}
+    public Fragment getmRightFragment() {
+        return mRightFragment;
+    }
 
-	public float getmLeftWeight() {
-		return mLeftWeight;
-	}
+    public Fragment getmLeftFragment() {
+        return mLeftFragment;
+    }
 
-	public float getmRightWeight() {
-		return mRightWeight;
-	}
+    public float getmLeftWeight() {
+        return mLeftWeight;
+    }
 
-	public int getFragmentsOrientation() {
-		return mFragmentsOrientation;
-	}
+    public float getmRightWeight() {
+        return mRightWeight;
+    }
 
-	protected void setSliderSize(int size) {
-		mRootPanel.setmSliderBarConst(size);
-	}
+    public int getFragmentsOrientation() {
+        return mFragmentsOrientation;
+    }
+
+    protected void setSliderSize(int size) {
+        mRootPanel.setmSliderBarConst(size);
+    }
 
 }
--- twoPanelsExample/src/main/java/com/example/twopanelsdemo/MainActivity.java
+++ twoPanelsExample/src/main/java/com/example/twopanelsdemo/MainActivity.java
@@ -10,48 +10,48 @@ import com.desarrollodroide.twopanels.TwoPanelsBaseActivity;
 
 public class MainActivity extends TwoPanelsBaseActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setBaseOrientation(LinearLayout.HORIZONTAL);
-		MyRightFragment mRightFragment = new MyRightFragment();
-		MyLeftFragment mLeftFragment = new MyLeftFragment();
-		getSupportFragmentManager().beginTransaction().add(R.id.right, mRightFragment).commit();
-		getSupportFragmentManager().beginTransaction().add(R.id.left, mLeftFragment).commit();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setBaseOrientation(LinearLayout.VERTICAL);
+        MyRightFragment mRightFragment = new MyRightFragment();
+        MyLeftFragment mLeftFragment = new MyLeftFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.right, mRightFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.left, mLeftFragment).commit();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu items for use in the action bar
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main_activity_actions, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int itemId = item.getItemId();
-		if (itemId == R.id.horizontal) {
-			setBaseOrientation(LinearLayout.HORIZONTAL);
-			updateButtonSliderOrientation();
-		} else if (itemId == R.id.vertical) {
-			setBaseOrientation(LinearLayout.VERTICAL);
-			updateButtonSliderOrientation();
-		} else if (itemId == R.id.hide_left_animate) {
-			slideFragmentsToLeft();
-		} else if (itemId == R.id.hide_right_animate) {
-			slideFragmentsToRight();
-		} else if (itemId == R.id.hide_left) {
-			hideLeft();
-		} else if (itemId == R.id.hide_right) {
-			hideRight();
-		} else if (itemId == R.id.show_two_fragments) {
-			showTwoFragments();
-		} else if (itemId == R.id.switch_slider) {
-			switchSliderVisitility();
-		} else {
-			return super.onOptionsItemSelected(item);
-		}
-		return true;
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.horizontal) {
+            setBaseOrientation(LinearLayout.HORIZONTAL);
+            updateButtonSliderOrientation();
+        } else if (itemId == R.id.vertical) {
+            setBaseOrientation(LinearLayout.VERTICAL);
+            updateButtonSliderOrientation();
+        } else if (itemId == R.id.hide_left_animate) {
+            slideFragmentsToLeft();
+        } else if (itemId == R.id.hide_right_animate) {
+            slideFragmentsToRight();
+        } else if (itemId == R.id.hide_left) {
+            hideLeft();
+        } else if (itemId == R.id.hide_right) {
+            hideRight();
+        } else if (itemId == R.id.show_two_fragments) {
+            showTwoFragments();
+        } else if (itemId == R.id.switch_slider) {
+            switchSliderVisitility();
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
 }
